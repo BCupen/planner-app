c1ab86c3cea07e802b48534b686715cef87ef37e
Bry Cupen <bacupen@gmail.com>
2025-04-14 18:45:46 -0400

    feat: display tasks for a given date

--- src/pages/SpecificDateTasks.tsx
+++ src/pages/SpecificDateTasks.tsx
@@ -1,8 +1,20 @@
 import { useParams } from "react-router";
 import { PageHeader } from "../components/PageHeader";
+import { TasksSection } from "../components/TasksSection";
+import { useAppSelector } from "../data/hooks";
+import { todoState } from "../data/todosSlice";
+import { useMemo } from "react";
+import { format, isEqual } from "date-fns";
 
 const SpecificDateTasks = () => {
   const { date } = useParams();
+  const todos = useAppSelector(todoState);
+  const datedTodos = useMemo(() => {
+    return todos.filter((todo) => {
+      const formatDate = format(todo.dueDate, "yyyy-MM-dd");
+      return isEqual(date || "", formatDate);
+    });
+  }, [todos, date]);
 
   return (
     <section className="flex flex-col items-start gap-5">
@@ -10,6 +22,8 @@ const SpecificDateTasks = () => {
         title={`${date}`}
         subText="Here are your tasks on this date"
       />
+
+      <TasksSection title="Tasks" todos={datedTodos} />
     </section>
   );
 };
