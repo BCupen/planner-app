3f026aeaaa02475d6982a4d04304277c6bdb859b
Bry Cupen <bacupen@gmail.com>
2025-04-30 12:02:05 -0400

    feat: persist user

--- src/data/userSlice.ts
+++ src/data/userSlice.ts
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+// userSlice.ts
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "./store";
 
 interface UserState {
@@ -6,22 +7,24 @@ interface UserState {
   email: string | null;
 }
 
-const initialState: UserState = {
-  name: null,
-  email: null,
-};
+const initialState: UserState = (() => {
+  const storedUser = localStorage.getItem("user");
+  return storedUser ? JSON.parse(storedUser) : { name: null, email: null };
+})();
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setUser: (state, action) => {
+    setUser: (state, action: PayloadAction<UserState>) => {
       state.name = action.payload.name;
       state.email = action.payload.email;
+      localStorage.setItem("user", JSON.stringify(action.payload)); // persist
     },
     removeUser: (state) => {
       state.name = null;
       state.email = null;
+      localStorage.removeItem("user"); // clear
     },
   },
 });
